be09efd57481c868ed00d86c81844bd04bd67f3a
ian <ian.page38@gmail.com>
2026-02-03 11:16:25 +0000

    update distorsion parameters

--- single_train.sh
+++ single_train.sh
@@ -1,10 +1,15 @@
-scene="custom_car"
-exp_name="test_1"
-gpu=-1
-ratio=1
-resolution=1
-appearance_dim=0
+#!/bin/bash
 
+scene="custom_car"
+exp_name="test_shiny_1"
+gpu=0
+
+# --- SHINY SURFACE OPTIMIZATION ---
+ratio=1
+resolution=1         # <--- MUST BE 1. '2' is too blurry for reflections.
+appearance_dim=32    # Keeps lighting consistent.
+
+# --- OCTREE SETTINGS ---
 fork=2
 base_layer=12
 visible_threshold=0.9
@@ -12,7 +17,7 @@ dist2level="round"
 update_ratio=0.2
 
 progressive="True"
-dist_ratio=0.999 #0.99
+dist_ratio=0.999
 levels=-1
 init_level=-1
 extra_ratio=0.25
@@ -33,7 +38,9 @@ if [ ! -z "$target_path" ]; then
     scene="$target_path"
 fi
 
+# Pass the new flags at the end
 ./train.sh -d ${scene} -l ${exp_name} --gpu ${gpu} -r ${resolution} --ratio ${ratio} --appearance_dim ${appearance_dim} \
    --fork ${fork} --visible_threshold ${visible_threshold} --base_layer ${base_layer} --dist2level ${dist2level} --update_ratio ${update_ratio} \
    --progressive ${progressive} --levels ${levels} --init_level ${init_level} --dist_ratio ${dist_ratio} \
-   --extra_ratio ${extra_ratio} --extra_up ${extra_up}
+   --extra_ratio ${extra_ratio} --extra_up ${extra_up} \
+   --undistorted --use_feat_bank
--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function rand(){
     min=$1
     max=$(($2-$min+1))
@@ -7,9 +9,14 @@ function rand(){
 
 port=$(rand 10000 30000)
 
-iterations=30_000
+iterations=30_000   # Increased for better shiny surfaces
 warmup="False"
 progressive="False"
+
+# Initialize empty variables for the flags
+undistorted=""
+use_feat_bank=""
+
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -l|--logdir) logdir="$2"; shift ;;
@@ -30,6 +37,12 @@ while [[ "$#" -gt 0 ]]; do
         --init_level) init_level="$2"; shift ;;
         --extra_ratio) extra_ratio="$2"; shift ;;
         --extra_up) extra_up="$2"; shift ;;
+
+        # --- NEW FLAGS ADDED HERE ---
+        --undistorted) undistorted="--undistorted" ;;
+        --use_feat_bank) use_feat_bank="--use_feat_bank" ;;
+        # ----------------------------
+
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -37,32 +50,24 @@ done
 
 time=$(date "+%Y-%m-%d_%H:%M:%S")
 
-if [ "$progressive" = "True" ]; then
-    if [ "$warmup" = "True" ]; then
-        python train.py -s ${data} -r ${resolution} --gpu ${gpu} --fork ${fork} --ratio ${ratio} --warmup \
-        --iterations ${iterations} --port $port -m outputs/${data}/${logdir}/$time --appearance_dim ${appearance_dim} \
-        --visible_threshold ${visible_threshold}  --base_layer ${base_layer} --dist2level ${dist2level} --update_ratio ${update_ratio} \
-        --progressive --init_level ${init_level} --dist_ratio ${dist_ratio} --levels ${levels}  \
-        --extra_ratio ${extra_ratio} --extra_up ${extra_up}
-    else
-        python train.py -s ${data} -r ${resolution} --gpu ${gpu} --fork ${fork} --ratio ${ratio} \
-        --iterations ${iterations} --port $port -m outputs/${data}/${logdir}/$time --appearance_dim ${appearance_dim} \
-        --visible_threshold ${visible_threshold}  --base_layer ${base_layer} --dist2level ${dist2level} --update_ratio ${update_ratio} \
-        --progressive --init_level ${init_level} --dist_ratio ${dist_ratio} --levels ${levels}  \
-        --extra_ratio ${extra_ratio} --extra_up ${extra_up}
-    fi
-else
-    if [ "$warmup" = "True" ]; then
-        python train.py -s ${data} -r ${resolution} --gpu ${gpu} --fork ${fork} --ratio ${ratio} --warmup \
-        --iterations ${iterations} --port $port -m outputs/${data}/${logdir}/$time --appearance_dim ${appearance_dim} \
-        --visible_threshold ${visible_threshold}  --base_layer ${base_layer} --dist2level ${dist2level} --update_ratio ${update_ratio} \
-        --init_level ${init_level} --dist_ratio ${dist_ratio} --levels ${levels}  \
-        --extra_ratio ${extra_ratio} --extra_up ${extra_up}
-    else
-        python train.py -s ${data} -r ${resolution} --gpu ${gpu} --fork ${fork} --ratio ${ratio} \
-        --iterations ${iterations} --port $port -m outputs/${data}/${logdir}/$time --appearance_dim ${appearance_dim} \
-        --visible_threshold ${visible_threshold}  --base_layer ${base_layer} --dist2level ${dist2level} --update_ratio ${update_ratio} \
-        --init_level ${init_level} --dist_ratio ${dist_ratio} --levels ${levels}  \
-        --extra_ratio ${extra_ratio} --extra_up ${extra_up}
-    fi
+# Combine the flags into one variable for cleaner code
+EXTRA_FLAGS="${undistorted} ${use_feat_bank}"
+
+# One clean command construction instead of 4 if/else blocks
+CMD="python train.py -s ${data} -r ${resolution} --gpu ${gpu} --fork ${fork} --ratio ${ratio} \
+--iterations ${iterations} --port $port -m outputs/${data##*/}/${logdir}/$time --appearance_dim ${appearance_dim} \
+--visible_threshold ${visible_threshold} --base_layer ${base_layer} --dist2level ${dist2level} --update_ratio ${update_ratio} \
+--init_level ${init_level} --dist_ratio ${dist_ratio} --levels ${levels} \
+--extra_ratio ${extra_ratio} --extra_up ${extra_up} \
+${EXTRA_FLAGS}"
+
+if [ "$warmup" = "True" ]; then
+    CMD="$CMD --warmup"
 fi
+
+if [ "$progressive" = "True" ]; then
+    CMD="$CMD --progressive"
+fi
+
+# Run it
+eval $CMD
